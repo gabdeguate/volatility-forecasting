e285f133147bf9cde522ef6bda6b60987fc308df
Seoyeon Park <gp8161072@gmail.com>
2026-05-12 15:03:40 +0900

    Update on Week 3 of project: Finished Garch, evaluated how lstm will be implemented and created lstm model

---
+++ models/lstm.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+
+class LSTMModel(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, dropout):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size = input_size,
+                            hidden_size= hidden_size, 
+                            num_layers=num_layers, 
+                            dropout=dropout, 
+                            batch_first= True)
+        self.output = nn.Linear(in_features=hidden_size, out_features = 1)
+    
+    def forward(self, x):
+        # x shape: (batch_size, seq_len, input_size)
+        # return shape: (batch_size, 1)
+        
+        out, _ =  self.lstm(x)
+        
+        pred = self.output(out[:,-1,:])
+        
+        return pred
+    
+        
+        
